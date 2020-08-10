080a3e0af8660ce099f353d356a69910ef3a6604
Daniel Bokor <daniel.bokor@connatix.com>
2020-08-10 12:06:05 +0300

    Stripped down TCString

--- modules/core/lib/TCString.d.ts
+++ modules/core/lib/TCString.d.ts
@@ -1,18 +1,9 @@
-import { EncodingOptions } from './encoder';
 import { TCModel } from './TCModel';
 /**
  * Main class for encoding and decoding a
  * TCF Transparency and Consent String
  */
 export declare class TCString {
-    /**
-     * encodes a model into a TCString
-     *
-     * @param {TCModel} tcModel - model to convert into encoded string
-     * @param {EncodingOptions} options - for encoding options other than default
-     * @return {string} - base64url encoded Transparency and Consent String
-     */
-    static encode(tcModel: TCModel, options?: EncodingOptions): string;
     /**
      * Decodes a string into a TCModel
      *
@@ -23,5 +14,5 @@ export declare class TCString {
      * none is passed a new instance of TCModel will be created.
      * @return {TCModel} - Returns populated TCModel
      */
-    static decode(encodedTCString: string, tcModel?: TCModel): TCModel;
+    static decode(encodedTCString: string): TCModel;
 }
--- modules/core/src/TCString.ts
+++ modules/core/src/TCString.ts
@@ -1,66 +1,18 @@
 import {
   Base64Url,
   BitLength,
-  EncodingOptions,
   SegmentEncoder,
-  SegmentSequence,
-  SemanticPreEncoder,
 } from './encoder';
 
-import {Segment, SegmentIDs} from './model';
-import {IntEncoder} from './encoder/field/IntEncoder';
-import {TCModel} from './TCModel';
+import { SegmentIDs } from './model';
+import { IntEncoder } from './encoder/field/IntEncoder';
+import { TCModel } from './TCModel';
 
 /**
  * Main class for encoding and decoding a
  * TCF Transparency and Consent String
  */
 export class TCString {
-
-  /**
-   * encodes a model into a TCString
-   *
-   * @param {TCModel} tcModel - model to convert into encoded string
-   * @param {EncodingOptions} options - for encoding options other than default
-   * @return {string} - base64url encoded Transparency and Consent String
-   */
-  public static encode(tcModel: TCModel, options?: EncodingOptions): string {
-
-    let out = '';
-    let sequence: Segment[];
-
-    tcModel = SemanticPreEncoder.process(tcModel, options);
-
-    /**
-       * If they pass in a special segment sequence.
-       */
-    if (Array.isArray(options?.segments)) {
-
-      sequence = options.segments;
-
-    } else {
-
-      sequence = new SegmentSequence(tcModel, options)[''+tcModel.version];
-
-    }
-
-    sequence.forEach((segment: Segment, idx: number): void => {
-
-      let dotMaybe = '';
-
-      if (idx < sequence.length - 1) {
-
-        dotMaybe = '.';
-
-      }
-
-      out += SegmentEncoder.encode(tcModel, segment) + dotMaybe;
-
-    });
-
-    return out;
-
-  }
 
   /**
    * Decodes a string into a TCModel
@@ -72,18 +24,14 @@ export class TCString {
    * none is passed a new instance of TCModel will be created.
    * @return {TCModel} - Returns populated TCModel
    */
-  public static decode(encodedTCString: string, tcModel?: TCModel): TCModel {
+  public static decode(encodedTCString: string): TCModel {
 
     const segments: string[] = encodedTCString.split('.');
     const len: number = segments.length;
 
-    if (!tcModel) {
+    const tcModel = new TCModel();
 
-      tcModel = new TCModel();
-
-    }
-
-    for (let i = 0; i < len; i ++) {
+    for (let i = 0; i < len; i++) {
 
       const segString: string = segments[i];
 
